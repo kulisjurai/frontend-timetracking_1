7169042f578c3b9465327da02c7fb58c628011c5
Kenan Novalić <72221119+kensi1995@users.noreply.github.com>
2022-12-12 09:00:10 +0100

    login register

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,13 @@
+import "./Login.css";
+
 function Login() {
   return (
-    <div>
-      <h1>this is login</h1>
+    <div className="login-form">
+      <h1>Login</h1>
+      <input type="email" placeholder="Email" />
+      <input type="text" placeholder="Passsword" />
+      <button className="login-btn">Login</button>
+      <p>Don't Have an Account! Sign in</p>
     </div>
   );
 }
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,15 @@
+import "./Register.css";
+
 function Register() {
   return (
-    <div>
-      <h1>this is register</h1>
+    <div className="register-form">
+      <h1>Register</h1>
+      <input type="text" placeholder="First Name" />
+      <input type="text" placeholder="Last Name" />
+      <input type="text" placeholder="Address" />
+      <input type="email" placeholder="Email" />
+      <input type="text" placeholder="Password" />
+      <button className="register-btn">Sign Up</button>
     </div>
   );
 }
